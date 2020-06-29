d435ca60eee15076a630802af8b5c27b1d6f17fa
dp4295 <deep@pdx.edu>
2020-06-28 22:16:32 -0700

    Added a unit test for ensuring the name of the student is in the toString() method

--- student/src/main/java/edu/pdx/cs410J/deep/Student.java
+++ student/src/main/java/edu/pdx/cs410J/deep/Student.java
@@ -11,22 +11,22 @@ public class Student extends Human {
                                                                                     
   /**                                                                               
    * Creates a new <code>Student</code>                                             
-   *                                                                                
-   * @param name                                                                    
-   *        The student's name                                                      
-   * @param classes                                                                 
-   *        The names of the classes the student is taking.  A student              
-   *        may take zero or more classes.                                          
-   * @param gpa                                                                     
-   *        The student's grade point average                                       
-   * @param gender                                                                  
-   *        The student's gender ("male" or "female", or "other", case insensitive)
-   */                                                                               
-  public Student(String name, ArrayList<String> classes, double gpa, String gender) {
-    super(name);
+   *  @param createStudentNamed
+   *        The student's name
+   * @param classes
+   *        The names of the classes the student is taking.  A student
+   *        may take zero or more classes.
+   * @param gpa
+ *        The student's grade point average
+   * @param gender
+   */
+
+    public Student(String createStudentNamed, ArrayList<String> classes, double gpa, String gender) {
+     super(createStudentNamed);
   }
 
-  /**                                                                               
+  /**
+   *
    * All students say "This class is too much work"
    */
   @Override
@@ -39,7 +39,9 @@ public class Student extends Human {
    * <code>Student</code>.                                                          
    */                                                                               
   public String toString() {
-    throw new UnsupportedOperationException("Not implemented yet");
+   // throw new UnsupportedOperationException("Not implemented yet");
+    return this.getName();
+
   }
 
   /**
