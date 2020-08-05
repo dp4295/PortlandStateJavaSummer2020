99a349cb82ad0c50fe95cb2d8a2a4674ab522c11
dp4295 <deep@pdx.edu>
2020-08-05 01:29:39 -0700

    Added PhoneBill-Android v1

---
+++ Phonebill-android/app/src/main/java/edu/pdx/cs410j/deep/phonebill/PhoneBill.java
@@ -0,0 +1,25 @@
+package edu.pdx.cs410j.deep.phonebill;
+
+import java.util.Collection;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.AbstractPhoneCall;
+
+public class PhoneBill extends AbstractPhoneBill {
+
+
+    @Override
+    public String getCustomer() {
+        return null;
+    }
+
+    @Override
+    public void addPhoneCall(AbstractPhoneCall call) {
+
+    }
+
+    @Override
+    public Collection getPhoneCalls() {
+        return null;
+    }
+}
